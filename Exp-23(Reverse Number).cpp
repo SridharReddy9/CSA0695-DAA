a4a1b9aa41f84fbc2d9f0efeefeb468d6d974a39
SridharReddy9 <113843187+SridharReddy9@users.noreply.github.com>
2023-08-30 09:55:46 +0530

    Add files via upload

---
+++ Exp-23(Reverse Number).cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,rev=0,r;
+	printf("Ente any Number : ");
+	scanf("%d",&a);
+	while(a!=0)
+	{
+		r=a%10;
+		rev=rev*10+r;
+		a=a/10;
+	}
+	printf("Reverse of given Number is : %d",rev);
+	return 0;
+}
